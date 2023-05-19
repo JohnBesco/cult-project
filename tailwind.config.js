b9266e4c54796e00c265d38ff29576d28b518877
John Besco <johnbesco.dev@gmail.com>
2023-05-19 19:36:22 +0100

    Add nav bar button and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,11 @@ module.exports = {
     },
     extend: {
       colors: {
+        white: '#FFFFFE',
         gold: '#CDB466',
         darkGold: '#DFA73E',
         brightOrange: '#FF7F00',
+        brightOrangeLight: '#FFA143',
         gradientGray: 'linear-gradient(90deg, #323435 0%, #0C0C0C 99.99%, #2A2B2C 100%, #2A2B2C 100%)'
       },
     },
