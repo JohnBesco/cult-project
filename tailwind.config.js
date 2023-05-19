6ec4c86f3cc917e07ab02d07a035b2a9104cd56e
John Besco <johnbesco.dev@gmail.com>
2023-05-19 15:17:26 +0100

    Add custom screen sizes & colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        gold: '#CDB466',
+        darkGold: '#DFA73E',
+        brightOrange: '#FF7F00'
+      }
+    },
   },
   plugins: [],
 }
