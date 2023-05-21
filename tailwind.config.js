26c110b3bd38434370a7241c4ca2e6358cd32f77
John Besco <johnbesco.dev@gmail.com>
2023-05-21 20:12:39 +0100

    Add first half hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,13 @@ module.exports = {
         darkGold: '#DFA73E',
         brightOrange: '#FF7F00',
         brightOrangeLight: '#FFA143',
-        gradientGray: 'linear-gradient(90deg, #323435 0%, #0C0C0C 99.99%, #2A2B2C 100%, #2A2B2C 100%)'
+        gradientGray: 'linear-gradient(90deg, #323435 0%, #0C0C0C 99.99%, #2A2B2C 100%, #2A2B2C 100%)',
+        grayText: '#6C6C6C'
       },
+      fontFamily: {
+        gothic: ['Germania One'],
+        cursive: ['Cinzel Decorative']
+      }
     },
   },
   plugins: [],
